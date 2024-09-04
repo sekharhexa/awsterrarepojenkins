9c07da3f54b05a05af07e2bb5dfc7cef54ed8530
sekharhexa <51188371+sekharhexa@users.noreply.github.com>
2024-09-04 16:45:10 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -46,8 +46,8 @@ resource "aws_eks_node_group" "demo" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 2
-    min_size     = 1
+    max_size     = 5
+    min_size     = 2
   }
 
   depends_on = [
